5cc16bd41efccd2c2fe27697b2e5a2b3d4bec2d3
Bhoomi Jii <bhumikaadhikari@Bhumikas-MacBook-Air.local>
2026-05-22 08:48:01 +0530

    fixed render port

--- js/server.js
+++ js/server.js
@@ -51,6 +51,8 @@ body: JSON.stringify({
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
